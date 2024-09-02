253eaae78394ef4d22e7b06fabfa902f5b363998
Vindass <jose.vindasrojas@ucr.ac.cr>
2024-09-02 16:54:16 -0600

    Actualización de las ventanas

---
+++ src/main/java/ucr/ac/cr/tm2100/g3/moveobject/view/PanelCanva.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ucr.ac.cr.tm2100.g3.moveobject.view;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author josev
+ */
+public class PanelCanva extends JPanel{
+    
+    private int x;
+    private int y;
+    
+    public PanelCanva() {
+        this.x=20;
+        this.y=20;
+    }
+    
+    @Override
+    protected void paintComponent (Graphics g) {
+        super.paintComponent (g);
+        g.setColor(Color.ORANGE);
+        g.fillOval(x, y, 25, 25);
+        g.fillRect(50, 100, 50, 30);
+    }
+    
+    
+}
